6bf81ce38787903ef7a5753a29eedb2575519a7e
Vitor shermon <victorshermon@yahoo.com>
2026-08-27 15:59:46 -0300

    refactor: move navItems to a separate const file

---
+++ apps/header/src/const.ts
@@ -0,0 +1,7 @@
+export const navItems = [
+  { label: "Inicio", path: "#inicio" },
+  { label: "Sobre", path: "#sobre" },
+  { label: "Serviços", path: "#servicos" },
+  { label: "Produtos", path: "#produtos" },
+  { label: "Contato", path: "#contato" },
+];
